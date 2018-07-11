24a0e2d43053522a49bb403ae5e62300b02895c8
Max Comet <maxime.comet@gmail.com>
2018-07-11 15:09:59 +0000

    test remember connexion

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable, :registerable, :validatable, :rememberable
-  devise :database_authenticatable,
+  devise :database_authenticatable, :rememberable, 
          :recoverable, :trackable
 end
